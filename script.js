cf09408a72528b849a65de2fd971e8cd5a040498
m879 <merajahmed2112@gmail.com>
2020-06-12 05:29:32 +0530

    word counter
    Simple word counter

--- script.js
+++
@@ -1,7 +0,0 @@
-function wordcount(){
-    var word = document.getElementById('txtarea').value;
-    var count = word.split(/[^\s]+/).length - 1;
-    document.getElementById("text").innerHTML=count;
-    var num=document.getElementById("get").value
-    document.getElementById("left").innerHTML=num-count;
-}
